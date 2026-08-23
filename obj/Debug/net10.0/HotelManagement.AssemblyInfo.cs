694fec55ec55c22d102946323164b3ebb2f4b473
MooHelmy <moohaamed.hhelmy.13@gmail.com>
2026-08-23 14:22:48 +0300

    add the setting of scalar

--- obj/Debug/net10.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4496f4018af3bf03cd427292535a83bc9c39abac")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
